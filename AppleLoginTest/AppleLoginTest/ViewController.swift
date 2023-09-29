c00338607ab575b1fdf5196f30b1527c73b1d956
junha <goodjunha@gmail.com>
2023-09-29 19:41:15 +0900

    🎱:: 세팅

---
+++ AppleLoginTest/AppleLoginTest/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  AppleLoginTest
+//
+//  Created by 박준하 on 2023/09/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
